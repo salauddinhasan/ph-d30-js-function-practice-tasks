8f189138bb22018f94b1e325b698fedbffe9fe20
salauddin <salauddin@hasan.com>
2026-02-01 22:38:11 +0600

    task 1 done

---
+++ function.js
@@ -0,0 +1,12 @@
+
+
+//  Task-1
+// Take four parameters. Multiply the four numbers and then return the result
+
+function numbers(num1, num2, num3, num4) {
+     return num1 * num2 * num3 * num4;
+}
+
+ const result = numbers(120, 23, 45 ,66);
+ console.log(result);
+ 
